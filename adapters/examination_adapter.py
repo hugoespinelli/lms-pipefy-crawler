29e68f84bab7b5cc3ac0feeb7b3b1b35f6566a4d
hugo-espinelli <hugoespinelli@gmail.com>
2020-09-27 11:22:46 -0300

    fix de valor vazio examinaation id

--- adapters/examination_adapter.py
+++ adapters/examination_adapter.py
@@ -10,11 +10,18 @@ class ExaminationAdapter:
         sort_func = lambda row: row["title"]
         table_rows = list(map(lambda row: row["node"], data))
         table_rows.sort(key=sort_func)
-        return {group: list(map(ExaminationAdapter.extract_examination, elements)) for group, elements in groupby(table_rows, sort_func)}
+        return {group: ExaminationAdapter.extract_examinations(elements) for group, elements in groupby(table_rows, sort_func)}
+
+    @staticmethod
+    def extract_examinations(elements):
+        examinations_ids = []
+        for row_data in elements:
+            examination_id = ExaminationAdapter.extract_examination(row_data)
+            if examination_id != "":
+                examinations_ids.append(examination_id)
 
     @staticmethod
     def extract_examination(row_data):
         record_fields = row_data["record_fields"]
-        examination_ids = list(filter(lambda record: record["name"] == EXAMINATION_FIELD, record_fields))[0]["value"]
-        return examination_ids if "" not in examination_ids else []
+        return list(filter(lambda record: record["name"] == EXAMINATION_FIELD, record_fields))[0]["value"]
 
