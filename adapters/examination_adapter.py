622934d93cf37006ca995e5813d1e0082f03fe24
hugo-espinelli <hugoespinelli@gmail.com>
2020-10-01 10:48:03 -0300

    fix de avaliacao lms nula

--- adapters/examination_adapter.py
+++ adapters/examination_adapter.py
@@ -11,16 +11,16 @@ class ExaminationAdapter:
         sort_func = lambda row: row["title"]
         table_rows = list(map(lambda row: row["node"], data))
         table_rows.sort(key=sort_func)
-        return {group: ExaminationAdapter.extract_examinations(elements) for group, elements in groupby(table_rows, sort_func)}
+        return {group: ExaminationAdapter.extract_examinations(list(elements)) for group, elements in groupby(table_rows, sort_func)}
 
     @staticmethod
     def extract_examinations(elements):
         examinations = []
         for row_data in elements:
             examination_dict = ExaminationAdapter.extract_examination(row_data)
-            if examination_dict["id_avaliacao_lms"] != "":
+            if EXAMINATION_FIELD in examination_dict and examination_dict[EXAMINATION_FIELD] != "":
                 examinations.append(Examination(
-                    id=examination_dict["id_avaliacao_lms"],
+                    id=examination_dict[EXAMINATION_FIELD],
                     min_score=0
                 ))
 
